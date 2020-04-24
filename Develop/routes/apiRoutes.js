a21659d0e8519029cad9dd03e401a15ffbefe25d
CJPALME <cjpalmerin@gmail.com>
2020-04-24 00:09:41 -0500

    Addedid and variable to catch req.body. Started creation of app.delete.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,28 +1,35 @@
 const fs = require("fs")
-const jsonNotes = require('./db/db.json')
+const jsonNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+
 
 module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
         res.json(jsonNotes)
-        // (fs.readFile('./db/db.json', (err, data) => {
+        // JSON.parse(fs.readFile('./db/db.json', (err, data) => {
         //     if (err) throw err;
         //     return JSON.parse(data);
         // });
     });
     app.post("/api/notes", function (req, res) {
-        // how to add ids everytime someone posts without using SQL
-        // let firstId = -1;
-        // let jsonNotesId = firstId +1
-        // jsonNotes["id"] = jsonNotesId
-        
+
+        let noteId = jsonNotes.length +1 
+        console.log(noteId);
+
+        let newNote = req.body;
+        newNote.id = noteId;
+
         // fs.appendFile('./db/db.json',req.body, (err) => {
         //     if (err) throw err;
         //     console.log("Success");
         // });
-        jsonNotes.push(req.body)
+
+        jsonNotes.push(newNote);
         res.json(jsonNotes)
     });
-    app.delete("/api/clear/:id", function (req, res) {
 
+    app.delete("/api/clear/:id", function (req, res) {
+        if(newNote.id === req.params.id) {
+            jsonNotes.splice(newNote.id, 1);
+        }
     });
 }
