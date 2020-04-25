f507634638c3c761aeec87e977fc0031bd78bcc7
CJPALME <cjpalmerin@gmail.com>
2020-04-24 19:14:38 -0500

    Finished teh delete api route and added id to test data.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,36 +1,34 @@
 const fs = require("fs")
 const jsonNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-
+console.log(jsonNotes);
 
 module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
         res.json(jsonNotes)
-        // JSON.parse(fs.readFile('./db/db.json', (err, data) => {
-        //     if (err) throw err;
-        //     return JSON.parse(data);
-        // });
+
     });
     app.post("/api/notes", function (req, res) {
 
-        let noteId = jsonNotes.length + 1
-        console.log(noteId);
+        let noteId = jsonNotes.length;
 
         let newNote = req.body;
         newNote.id = noteId;
-
-        // fs.appendFile('./db/db.json',req.body, (err) => {
-        //     if (err) throw err;
-        //     console.log("Success");
-        // });
 
         jsonNotes.push(newNote);
         res.json(jsonNotes)
     });
 
-    app.delete("/api/clear/:id", function (req, res) {
-        if (newNote.id === req.params.id) {
-            jsonNotes.splice(newNote.id, 1);
+    app.delete("/api/notes/:id", function (req, res) {
+
+        reqParamsNum = parseInt(req.params.id)
+
+        for(let i = 0; i < jsonNotes.length; i++) {
+            
+            if(reqParamsNum === jsonNotes[i].id) {
+                jsonNotes.splice(i, 1)
+            }
         }
         res.json(jsonNotes)
-    });
-}
+    })};
+
+// db test data: [{"title":"Note Title","text":"Note", "id" : 0}]
